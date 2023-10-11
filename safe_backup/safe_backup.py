50a7ad1ea37402da07b37fed1ce9ca4ab035ded1
Vahidreza Naderi <vahidrnaderi@gmail.com>
2023-10-11 16:51:48 +0200

    Complete args menu and saving source's files list in redis

--- safe_backup/safe_backup.py
+++ safe_backup/safe_backup.py
@@ -36,12 +36,18 @@ AWS_ENDPOINT_URL = os.environ['AWS_ENDPOINT_URL']
 class SafeBackup:
 	redis_db = redis.StrictRedis(host="localhost", port=6379, db=0)
 
-	def get_files_list(self, source, location):
+	def save_files_list_in_redis(self, source, location):
 		"""
-			source must be:		local 				|	s3
-			location must be:	source_directory	|	bucket_name
+			Make a list from source and save it in redis.
+			source must be one of 'local' or 's3'
+			
+			if source is 'local' then
+					<location> must be <source_directory>	
+			and if source is 's3' then
+					<location> must be <bucket_name>
 		"""
 		files_path=""
+		redis_key = ""
 		match source:
 			case 's3':
 				logging.debug("Source is a s3.")
@@ -61,7 +67,8 @@ class SafeBackup:
 					bucket = s3_re.Bucket(bucket.name)
 					for obj in bucket.objects.all():
 						logging.debug(f"{obj.key} {obj.last_modified}")
-						self.redis_db.sadd(f"{source}-{location}", obj.key)
+						redis_key = f"{source}-{location}"
+						self.redis_db.sadd(redis_key, obj.key)
 
 			case 'local':
 				logging.debug("Source is a local.")
@@ -93,42 +100,74 @@ class SafeBackup:
 							logging.debug('FILE INSIDE ' + files_path + ': '+ filename)
 							file_path = f"{files_path}/{filename}"
 							# self.redis_db.rpush(root_path, file_path)
-							self.redis_db.sadd(f"{source}-{root_path}", file_path)
+							redis_key = f"{source}-{root_path}"
+							self.redis_db.sadd(redis_key, file_path)
 					logging.debug(self.redis_db.keys())
 				else:
 					print("location is not directory or not exist.")
 					exit(1)
 			case _:
 				print("Source is not valied.")
+		return redis_key
 
 
-def main(args):
+
+
+
+def main():
 	parser = argparse.ArgumentParser(
 				prog='safe_backup',
 				description='Backup your local or s3 files safely.')
 
-	
-	parser.add_argument('source', metavar=('<SOURCE_TYPE>'), 
-				help= 'get source type [\'local\' | \'s3\']')
-	parser.add_argument('location', metavar=('<SOURCE_ADDRESS>'), 
-				help= 'get [ <SOURCE_DIRECTORY> | <BUCKET_NAME> ] to create list of source files in Redis')
-	parser.add_argument('-c', nargs=1, metavar=('<DEST_DIRECTORY>'),
-				help= 'copy source files to destination')	
+	group = parser.add_mutually_exclusive_group(required=True)
+		
+	group.add_argument('-l', nargs=2, metavar=('<SOURCE_TYPE>', '<SOURCE_ADDRESS>'), 
+				help= 'get <SOURCE_TYPE> as [\'local\' | \'s3\'] \
+				and [ <SOURCE_DIRECTORY> | <BUCKET_NAME> ] \
+				to create list of source files in Redis')
+	group.add_argument('-c', nargs=5, metavar=('<SOURCE_TYPE>', \
+				'<SOURCE_ADDRESS>', '<DEST_DIRECTORY>', '<REDIS_KEY>', 
+				'<NUMBER_OF_WORKERS>'),
+				help= 'get <SOURCE_TYPE> as [\'local\' | \'s3\'] \
+				and [ <SOURCE_DIRECTORY> | <BUCKET_NAME> ] \
+				then copy source files to destination')
+	group.add_argument('-d', nargs=3, metavar=('<REDIS_KEY>', 
+				'<DEST_DIRECTORY>', '<NUMBER_OF_WORKERS>'),
+				help= 'read Redis and download source files safely to destination')	
 	
 	args = parser.parse_args()
 	logging.debug(args)	
-	logging.debug(args.source)
-	logging.debug(args.location)
+	logging.debug(args.l)
 	logging.debug(args.c)
+	logging.debug(args.d)
 	
 	safe_backup = SafeBackup()
-	if args.c == None:
-		safe_backup.get_files_list(args.source,args.location)
+	
+	if args.l:
+		if not args.l[0]=='local' and not args.l[0]=='s3':
+			parser.error(f"<SOURCE_TYPE> must be one of 'local' or 's3'")
+		if args.l[0]=='local' and not Path(args.l[1]).is_dir():
+			parser.error(f"<SOURCE_ADDRESS>='{args.l[1]}' is not directory or not exist!")
+		result = safe_backup.save_files_list_in_redis(args.l[0],args.l[1])
+		print(f"List of files created in '{result}' redis key successfuly.")
+	elif args.c:
+		if not args.c[0]=='local' and not args.c[0]=='s3':
+			parser.error(f"<SOURCE_TYPE> must be one of 'local' or 's3'")
+		if args.c[0]=='local' and not Path(args.c[1]).is_dir():
+			parser.error(f"<SOURCE_ADDRESS>='{args.c[1]}' is not directory or not exist!")
+		if not Path(args.c[2]).is_dir():
+			parser.error(f"<DEST_DIRECTORY>='{args.c[2]}' is not directory or not exist!")
+	elif args.d:
+		if not Path(args.d[1]).is_dir():
+			parser.error(f"<DEST_DIRECTORY>='{args.d[1]}' is not directory or not exist!")
+		if not args.d[2].isdigit() or int(args.d[2])<=0:
+			parser.error(f"<NUMBER_OF_WORKERS>='{args.d[2]}' is not integer or not bigger than 0!")
 	else:
-		pass
+		parser.error(f"Input args='{args}' is not defined!")
+		
 
 	return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit(main())
