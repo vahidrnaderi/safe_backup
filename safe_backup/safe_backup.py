83b7d97fd794afc7fa8092e317eb16100f974fbb
Vahidreza Naderi <vahidrnaderi@gmail.com>
2023-10-26 23:58:02 +0200

    Create bucket_exist function for menu

--- safe_backup/safe_backup.py
+++ safe_backup/safe_backup.py
@@ -27,6 +27,7 @@ import redis
 import argparse
 from multiprocessing import Pool
 import functools
+import time
 
 
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - \
@@ -135,10 +136,8 @@ class SafeBackup:
 		# Create bucket
 		try:
 			if region is None:
-				# s3_client = boto3.client('s3')
 				s3_client.create_bucket(Bucket=bucket_name)
 			else:
-				# s3_client = boto3.client('s3', region_name=region)
 				location = {'LocationConstraint': region}
 				s3_client.create_bucket(Bucket=bucket_name,
 										CreateBucketConfiguration=location)
@@ -187,8 +186,25 @@ class SafeBackup:
 			if 'Contents' in list(page.keys()):
 				self.redis_db.set(f"{redis_key}:{command_key}:marker", page['Marker'])	
 				self.__pooled__(redis_key, page['Contents'])
+				time.sleep(5)
 		else:
 			self.redis_db.getdel(f"{redis_key}:{command_key}:marker")
+
+	@debug
+	def bucket_exists(self, bucket_name):
+		s3_so = self.__s3_connect__()
+		bucket = s3_so.Bucket(bucket_name)
+		try:
+			s3_so.meta.client.head_bucket(Bucket=bucket_name)
+			return True, f"<BUCKET_NAME>='{bucket_name}' is exists!"
+		except ClientError as e:
+			# If a client error is thrown, then check that it was a 404 error.
+			# If it was a 404 error, then the bucket does not exist.
+			error_code = e.response['Error']['Code']
+			if error_code == '404':	
+				return False, f"<BUCKET_NAME>='{bucket_name}' is not exists!"
+			else:	
+				return False, f"Something went wrong in s3:<BUCKET_NAME>='{bucket_name}'! Error is {e}"	
 	
 	@debug	
 	def save_files_list_in_redis(self, option, source, location):
@@ -241,10 +257,9 @@ class SafeBackup:
 						for filename in filenames:
 							logging.debug(' *** save_files_...() => FILE INSIDE ' + files_path + ': '+ filename)
 							file_path = f"{files_path}/{filename}"
-							# self.redis_db.rpush(root_path, file_path)
-							# redis_key = f"{source}:{root_path}"
 							redis_key = f"{source}:{location}"
 							self.redis_db.sadd(redis_key, file_path)
+						time.sleep(5)
 					logging.debug(f" *** save_files_...() => {self.redis_db.keys()}")					
 					print(f"List of files created in '{redis_key}' redis key successfuly.")
 				else:
@@ -285,7 +300,6 @@ class SafeBackup:
 								member.decode('UTF-8'),
 								f"{destination}/{member.decode('UTF-8')}")
 							self.redis_db.srem(f"{redis_key}-{option}__{destination}__{workers}-working", member)
-							# time.sleep(5)
 						except Exception as e:
 							print(f"There was an error: {e}")
 							self.redis_db.smove(f"{redis_key}-{option}__{destination}__{workers}-working",
@@ -293,7 +307,7 @@ class SafeBackup:
 							
 			elif redis_key.startswith('s3:') and destination.startswith('s3:'):
 				s3_dest_bucket=destination.split(':')[1]
-				logging.debug(f" *** l-188 {member.decode('UTF-8')} --> s3:{s3_dest_bucket}")
+				logging.debug(f" *** {member.decode('UTF-8')} --> s3:{s3_dest_bucket}")
 				parent = Path(f"./{destination}/{member.decode('UTF-8')}").parent
 				if not os.path.exists(parent):
 					os.makedirs(parent)
@@ -301,22 +315,20 @@ class SafeBackup:
 				
 				# download from s3 source
 				s3_source = self.__s3_connect__().meta.client
-				logging.debug(f" *** l-197 {source[1]} -> ./{destination}/{member.decode('UTF-8')}")
+				logging.debug(f" *** {source[1]} -> ./{destination}/{member.decode('UTF-8')}")
 				try:
-					logging.debug(f" *** l-199")
 					s3_source.download_file(source[1],
 						member.decode('UTF-8'),
 						f"./{destination}/{member.decode('UTF-8')}")
 				except Exception as e:
 					print(f" There was an error: {e}")
 					self.redis_db.smove(f"{redis_key}-{option}__{destination}__{workers}-working",
-						redis_key, member)				
-				logging.debug(f" *** l-207")
+						redis_key, member)
 				
 				# upload to s3 destination	
 				s3_dest = self.__s3_connect__('dest').meta.client
 				try:
-					logging.debug(f" *** l-212 {s3_dest.list_buckets()['Buckets']}")
+					logging.debug(f" *** {s3_dest.list_buckets()['Buckets']}")
 					s3_dest.head_bucket(Bucket=s3_dest_bucket)
 				except ClientError:
 					# The bucket does not exist or you have no access.
@@ -324,10 +336,9 @@ class SafeBackup:
 						print("  ######  There was a problem to create destination bucket!")	
 						exit(1)
 				else:					
-					logging.debug(f" *** l-220 {member.decode('UTF-8')} -> {member.decode('UTF-8')}")
+					logging.debug(f" *** {member.decode('UTF-8')} -> {member.decode('UTF-8')}")
 					if os.path.exists(Path(f"./{destination}/{member.decode('UTF-8')}")):
 						try:
-							logging.debug(f" *** l-222")
 							s3_dest.upload_file(f"./{destination}/{member.decode('UTF-8')}",
 								s3_dest_bucket, member.decode('UTF-8'))
 							self.redis_db.srem(f"{redis_key}-{option}__{destination}__{workers}-working", member)
@@ -343,12 +354,12 @@ class SafeBackup:
 							elif os.path.isfile(path): 
 								os.unlink(path)																			
 					else:
-						print(f" The file ./{destination}/{member.decode('UTF-8')} not exists!")					
-					logging.debug(f" *** l-220")	
+						print(f" The file ./{destination}/{member.decode('UTF-8')} not exists!")
 				
 			else:
 				print(" There was a problem for copy s3 to s3!")	
-				exit(2)	
+				exit(2)
+			time.sleep(5)	
 		else:
 			self.redis_db.getdel(f"{redis_key}-{option}__{destination}__{workers}-working1")
 			
@@ -393,6 +404,10 @@ def main():
 			parser.error(f"<SOURCE_TYPE> must be one of 'local' or 's3'")
 		if args.l[0]=='local' and not Path(args.l[1]).is_dir():
 			parser.error(f"<SOURCE_ADDRESS>='{args.l[1]}' is not directory or not exist!")
+		if args.l[0]=='s3':
+			result, msg = safe_backup.bucket_exists(args.l[1])
+			if not result:
+				parser.error(msg)
 		
 		"Make list of source files to Redis"
 		safe_backup.save_files_list_in_redis('l', args.l[0], args.l[1])
@@ -402,7 +417,11 @@ def main():
 			parser.error(f"<SOURCE_TYPE> must be one of 'local' or 's3'")
 		if args.c[0]=='local' and not Path(args.c[1]).is_dir():
 			parser.error(f"<SOURCE_ADDRESS>='{args.c[1]}' is not \
-			directory or not exist!")
+			directory or not exist!")		
+		if args.l[0]=='s3':
+			result, msg = safe_backup.bucket_exists(args.l[1])
+			if not result:
+				parser.error(msg)
 		if not Path(args.c[2]).is_dir():
 			parser.error(f"<DEST_DIRECTORY>='{args.c[2]}' is not \
 			directory or not exist!")
