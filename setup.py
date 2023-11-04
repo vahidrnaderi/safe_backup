826a1c4f75a2cb37500f930bb43be509e0001bf2
Vahidreza Naderi <vahidrnaderi@gmail.com>
2023-11-05 00:25:00 +0100

    Make setup.py for 'sbackup' command

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='safe_backup',
+    version='0.1.0',
+    packages=find_packages(include=['safe_backup', 'safe_backup.*']),
+    install_requires=[
+        'async-timeout==4.0.3',
+        'boto3==1.28.62',
+        'botocore==1.31.62',
+        'certifi==2023.7.22',
+        'jmespath==1.0.1',
+        'minio==7.1.17',
+        'python-dateutil==2.8.2',
+        'redis==5.0.0',
+        's3transfer==0.7.0',
+        'six==1.16.0',
+        'urllib3==2.0.5',
+    ],
+    entry_points={
+        'console_scripts': ['sbackup=safe_backup.safe_backup:main']
+    },
+)
